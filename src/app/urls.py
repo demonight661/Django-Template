cd4bcbd780d11075b77dce1d7effb029701a3bb4
Zander404 <alexandrearthurpg@gmail.com>
2023-01-20 17:48:32 -0300

    config urls path

--- src/app/urls.py
+++ src/app/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('port/', include("port.urls")),
+    path('', include("port.urls")),
     
 ]
